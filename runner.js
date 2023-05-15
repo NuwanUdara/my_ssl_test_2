2cdc79f60b97e83d43c2d99da935f611d08d7658
nuwanudara <nuwanudara36@gmail.com>
2023-05-15 11:57:23 +0530

    asdsd

--- runner.js
+++ runner.js
@@ -2,21 +2,22 @@
 const run = require('./d.js');
 
 let options={};
-
-async function walk(){
-    let ff = await run()
-    console.log(ff, typeof(ff))
-    options = {
-        //key: fs.readFileSync("key.pem"),
-        //cert: fs.readFileSync("cert.pem"),
-        //ca: fs.readFileSync("ca.pem"),
-        ca:ff.toString(),
-        requestCert: true,
-        rejectUnauthorized: true
-        };
+let ff = await run()
+console.log(ff, typeof(ff))
+// async function walk(){
+//     let ff = await run()
+//     console.log(ff, typeof(ff))
+//     options = {
+//         //key: fs.readFileSync("key.pem"),
+//         //cert: fs.readFileSync("cert.pem"),
+//         //ca: fs.readFileSync("ca.pem"),
+//         ca:ff.toString(),
+//         requestCert: true,
+//         rejectUnauthorized: true
+//         };
     
-        console.log(options);       
-}
-walk()
+//         console.log(options);       
+// }
+// walk()
 
 
