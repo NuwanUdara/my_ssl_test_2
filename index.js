feb34c69a0532870f6f411f907d963d5fff4d4f7
nuwanudara <nuwanudara36@gmail.com>
2023-05-15 10:00:31 +0530

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+// Requiring in-built https for creating
+// https server
+
+const https = require("https");
+
+// Express for handling GET and POST request
+const express = require("express");
+const app = express();
+
+// Requiring file system to use local files
+const fs = require("fs");
+
+
+// Get request for root of the app
+app.get("/", function (req, res) {
+
+res.send("welcome Home")
+});
+
+// Creating object of key and certificate
+// for SSL
+const options = {
+key: fs.readFileSync("key.pem"),
+cert: fs.readFileSync("cert.pem"),
+ca: fs.readFileSync("ca.pem"),
+requestCert: true,
+rejectUnauthorized: true
+};
+
+// Creating https server by passing
+// options and app object
+https.createServer(options, app)
+.listen(443, function (req, res) {
+console.log("Server started at port 443");
+});
---
+++ runner.js
@@ -0,0 +1,6 @@
+// import {run} from './d.js';
+const run = require('./d.js');
+
+let ff = run();
+
+console.log(ff)
