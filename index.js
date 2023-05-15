82ff76d0a543ec4861a861ca4a548c1de4237454
nuwanudara <nuwanudara36@gmail.com>
2023-05-15 12:01:26 +0530

    index update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function walk(){
         key: fs.readFileSync("key.pem"),
         cert: fs.readFileSync("cert.pem"),
         //ca: fs.readFileSync("ca.pem"),
-        ca:ff.toString(),
+        ca:ff,
         requestCert: true,
         rejectUnauthorized: true
         };
