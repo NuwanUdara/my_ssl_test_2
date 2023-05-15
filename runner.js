23dd83e56c9671f344aa862ea9b0c8bc920052c3
nuwanudara <nuwanudara36@gmail.com>
2023-05-15 12:00:33 +0530

    rdtfcygvuhb

--- runner.js
+++ runner.js
@@ -1,26 +1,26 @@
 // import {run} from './d.js';
 const run = require('./d.js');
 
-let options={};
-async function walk(){
-    let ff = await run()
-    console.log(ff, typeof(ff))
-}
-walk()
+
 // async function walk(){
 //     let ff = await run()
 //     console.log(ff, typeof(ff))
-//     options = {
-//         //key: fs.readFileSync("key.pem"),
-//         //cert: fs.readFileSync("cert.pem"),
-//         //ca: fs.readFileSync("ca.pem"),
-//         ca:ff.toString(),
-//         requestCert: true,
-//         rejectUnauthorized: true
-//         };
-    
-//         console.log(options);       
 // }
+walk()
+async function walk(){
+    let ff = await run()
+    console.log(ff, typeof(ff))
+    let options = {
+        //key: fs.readFileSync("key.pem"),
+        //cert: fs.readFileSync("cert.pem"),
+        //ca: fs.readFileSync("ca.pem"),
+        ca:ff,
+        requestCert: true,
+        rejectUnauthorized: true
+        };
+    
+        console.log(options);       
+}
 // walk()
 
 
